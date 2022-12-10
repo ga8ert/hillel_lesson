d37671d390801ad405e2e8c0641134b2a3f0fb25
ga8ert <ga8ert@protonmail.com>
2022-12-10 20:24:49 +0200

    ...

--- lesson10/task10/library.py
+++ lesson10/task10/library.py
@@ -2,9 +2,7 @@ import functools
 
 
 @functools.cache
-def exponent_mult(num1: int | float,
-                  num2: int | float, 
-                  exp: int | float) -> int | float:
+def exponent_mult(num1: int | float, num2: int | float, exp: int | float) -> int | float:
     mult = num1 * num2
     result = mult ** exp
     return result
